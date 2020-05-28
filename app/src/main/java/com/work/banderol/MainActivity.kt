092a6af77a0ed3b20ecdd3c676672e519d143fe7
andreilemdyanov <lemdyanov5@mail.ru>
2020-05-28 21:29:27 +0300

    add and check permission READ_CONTACTS.

--- app/src/main/java/com/work/banderol/MainActivity.kt
+++ app/src/main/java/com/work/banderol/MainActivity.kt
@@ -3,10 +3,12 @@ package com.work.banderol
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.theartofdev.edmodo.cropper.CropImage
 import com.work.banderol.databinding.ActivityMainBinding
 import com.work.banderol.model.User
@@ -28,10 +30,17 @@ class MainActivity : AppCompatActivity() {
         APP_ACTIVITY = this
         initFirebase()
         initUser {
+            initContacts()
             initFields()
             initFunc()
         }
 
+    }
+
+    private fun initContacts() {
+        if (checkPermission(READ_CONTACTS)) {
+            showToast("Чтение контактов")
+        }
     }
 
     override fun onStart() {
@@ -52,13 +61,27 @@ class MainActivity : AppCompatActivity() {
         } else {
             replaceActivity(RegisterActivity())
         }
-
     }
 
     private fun initFields() {
         mToolbar = mBinding.mainToolbar
         mAppDrawer = AppDrawer(this, mToolbar)
 
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (ContextCompat.checkSelfPermission(
+                APP_ACTIVITY,
+                READ_CONTACTS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            initContacts()
+        }
     }
 
 }
